d202421d90979feeffc4d967981c7e554956254e
ShaoXiangChien <79143079+ShaoXiangChien@users.noreply.github.com>
2022-05-04 20:37:54 +0800

    first commit

---
+++ data_collection.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import requests
+from pprint import pprint
+import time
+import.google_search
+
+
+def parse_content(source, piece):
+    response = google_search.get_source(piece['link'])
+    title = ''
+    date = ''
+    article_contents = ''
+    if source == 'udn.com':
+        pass
+    elif source == 'chinatimes.com':
+        pass
+    elif source == 'news.tvbs.com':
+        date = response.html.xpath("//div[@class='author']/br/text()")[0]
+        article_contents = response.html.xpath(
+            "//div[@class='article_content']/text()")
+    elif source == 'setn.com':
+        pass
+    elif source == 'ltn.com':
+        date = response.html.xpath("//span[@class='time']/text()")
+        article_contents = response.html.xpath(
+            "//div[@class='text boxTitle boxText']/p[not(@*)]/text()")
+    elif source == 'appledaily.com':
+        date = response.html.xpath("//div[@class='timestamp']/text()[2]")[0]
+        article_contents = response.html.xpath("//section/p/text()")
+        print(article_contents)
+
+    return {'title': piece['title'], 'date': date, 'paragraph': ' '.join(article_contents), 'source': source}
+
+    # return info
+
+
+def collect_data(query):
+    results = google_search.google_search(query)
+    target_sources = ['udn.com', 'chinatimes.com',
+                      'news.tvbs.com', 'setn.com', 'ltn.com', 'appledaily.com']
+    desired_news = {ta: [] for ta in target_sources}
+    for res in results:
+        for ta in target_sources:
+            if ta in res['link']:
+                desired_news[ta].append(res)
+                break
+    return desired_news, len(results)
+
+
+if __name__ == '__main__':
+    tic = time.perf_counter()
+    news, num = collect_data('萊豬')
+    toc = time.perf_counter()
+    print(f"Collect the data in {toc - tic:0.4f} seconds")
+    print(f'# of results: {num}')
+    news_df = pd.DataFrame()
+    for k, v in news.items():
+        print(k, len(v))
+        for piece in v:
+            news_df = news_df.append(parse_content(
+                k, piece), ignore_index=True)
+    print(news_df)
+    news_df.to_csv('output.csv', index=False)
---
+++ google_search.py
@@ -0,0 +1,90 @@
+import requests
+import urllib
+import pandas as pd
+from lxml import etree
+from requests_html import HTML
+from requests_html import HTMLSession
+
+
+def get_source(url):
+    """Return the source code for the provided URL.
+
+    Args:
+        url (string): URL of the page to scrape.
+
+    Returns:
+        response (object): HTTP response object from requests_html.
+    """
+
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+
+def get_results(query):
+
+    query = urllib.parse.quote_plus(query)
+    response = get_source(
+        "https://www.google.com/search?q=" + query + '&filter=0')
+    responses = [response]
+
+
+0):
+        url=response.html.xpath(f"//a[@aria-label='Page {i}']/@href")
+        if len(url):
+            new_res=get_source("https://www.google.com" + url[0])
+            responses.append(new_res)
+            if i == 10 or ((i - 10) % 4 == 0 and i > 10):
+                response=new_res
+        else:
+            break
+
+    return responses
+
+
+def parse_results(response):
+
+    css_identifier_result=".tF2Cxc"
+    css_identifier_title="h3"
+    css_identifier_link=".yuRUbf a"
+    css_identifier_text=".VwiC3b"
+
+    results=response.html.find(css_identifier_result)
+
+    output=[]
+
+    for result in results:
+        try:
+            title=result.find(css_identifier_title, first = True).text
+        except:
+            title=''
+        try:
+            link=result.find(css_identifier_link, first = True).attrs['href']
+        except:
+            link=''
+        try:
+            text=result.find(css_identifier_text, first = True).text
+        except:
+            text=''
+
+        item={
+            'title': title,
+            'link': link,
+            'text': text
+        }
+
+        output.append(item)
+
+    return output
+
+
+def google_search(query):
+    responses = get_results(query)
+    results = []
+    for response in responses:
+        results += parse_results(response)
+    return results
