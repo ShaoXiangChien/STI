dc134cd956cfd9a614e09c0656cf22952b0360a1
ShaoXiangChien <79143079+ShaoXiangChien@users.noreply.github.com>
2022-08-21 08:30:24 +0800

    update bertopic service

--- berTopic_service.py
+++ berTopic_service.py
@@ -5,8 +5,9 @@ def topic_modeling(df):
     model = BERTopic(language="Chinese (Traditional)",
                      calculate_probabilities=True,
                      verbose=True)
-
-    docs = df['title_tokens'].to_list()
+    df['full_tokens'] = df.apply(
+        lambda row: row['title_tokens'] + " " + row['paragraph_tokens'], axis=1)
+    docs = df['full_tokens'].to_list()
     topics, probs = model.fit_transform(docs)
     df['topic'] = topics
     topic_kws = {}
