7061cb97af4e2363a238e2a05612f92d0c077128
ShaoXiangChien <79143079+ShaoXiangChien@users.noreply.github.com>
2022-09-11 22:19:59 +0800

    add summary metrics

--- metrics.py
+++ metrics.py
@@ -16,3 +16,27 @@ def keyword_precision_eval(ans: list, obj: list) -> float:
         if kw in ans:
             hit += 1
     return hit, hit / len(obj)
+
+
+def summary_precision(output: str, human: str) -> float:
+    hit = 0
+    for w in output:
+        if w in human:
+            hit += 1
+
+    return hit / len(output)
+
+
+def summary_recall(output: str, human: str) -> float:
+    hit = 0
+    for w in human:
+        if w in output:
+            hit += 1
+    return hit / len(human)
+
+
+def summary_f1_eval(output: str, human: str) -> float:
+    precision = summary_precision(output, human)
+    recall = summary_recall(output, human)
+    f1 = 2 * (precision * recall) / (precision + recall)
+    return f1
